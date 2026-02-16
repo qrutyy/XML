3f8b5e98fe4911d4a66ab44983f54d180913360f
Vlasenco Daniel <vlasenko.daniil26@gmail.com>
2026-02-16 15:26:58 +0300

    feat: added cram test showing llvm codegen options

--- XML/lib/backend/codegen_llvm.mli
+++ XML/lib/backend/codegen_llvm.mli
@@ -2,8 +2,6 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-open Format
-
 (** [gen_program_ir prog triple opt] gens program in LLMV IR from the program [prog]
 for the target architecture specified by [triple] with optimization level [opt] if not None, O0 otherwise*)
 val gen_program_ir : Middleend.Anf.aprogram -> string -> string option -> string
